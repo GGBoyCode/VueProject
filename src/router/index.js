b25689baa900d5f7d6fc586a80e88165172f80db
GGBoyCode <1770061644@qq.com>
2020-11-30 21:08:48 +0800

    添加用户页面相关路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,12 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const Home = () => import('../views/Home');
-const Profile = () => import('../views/Profile');
+const Home = () => import('../views/home/Home');
+const Profile = () => import('../views/profile/Profile');
+const AccountSetting = () => import('../views/profile/page/AccountSetting');
+const Collection = () => import('../views/profile/page/Collection');
+const Connect = () => import('../views/profile/page/Connect');
+const Message = () => import('../views/profile/page/Message');
 
 const routes = [
     //如果为'/home'则重定向为根目录'/'
@@ -20,7 +24,34 @@ const routes = [
   {
     path: '/profile',
     name: 'profile',
-    component: Profile
+    component: Profile,
+    children:[
+      {
+        path:'',
+        redirect:'account'
+      },
+      {
+        path: 'account',
+        name: 'account',
+        component: AccountSetting
+      },
+      {
+        path: 'collection',
+        name: 'collection',
+        component: Collection
+      },
+      {
+        path: 'connect',
+        name: 'connect',
+        component: Connect
+      },
+      {
+        path: 'message',
+        name: 'message',
+        component: Message
+      }
+
+    ]
   }
 ]
 
