8a0b980feb3de224bb5e04601474ddd34750049a
GGBoyCode <1770061644@qq.com>
2020-12-03 18:55:12 +0800

    完善用户信息页面

--- src/views/profile/page/AccountSetting.vue
+++ src/views/profile/page/AccountSetting.vue
@@ -67,7 +67,9 @@
                 <el-col :span="12">
                     {{$store.state.user.telephone}}
                 </el-col>
-                <el-col :span="6"><el-button style="float: right;" size="mini" type="primary">更换手机</el-button></el-col>
+                <el-col :span="6">
+                    <el-button style="float: right;" size="mini" type="primary" @click="openDialog('绑定手机')">更换手机</el-button>
+                </el-col>
             </el-row>
 
             <el-row style="height: 50px" type="flex" align="middle">
@@ -77,7 +79,9 @@
                 <el-col :span="12">
                     {{$store.state.user.email}}
                 </el-col>
-                <el-col :span="6"><el-button style="float: right;" size="mini" type="primary">更换邮箱</el-button></el-col>
+                <el-col :span="6">
+                    <el-button style="float: right;" size="mini" type="primary" @click="openDialog('绑定邮箱')">更换邮箱</el-button>
+                </el-col>
             </el-row>
 
             <el-row style="height: 50px" type="flex" align="middle">
@@ -87,6 +91,44 @@
                 <el-col :span="12">&nbsp;</el-col>
                 <el-col :span="6"><el-button style="float: right;" size="mini" type="primary">修改密码</el-button></el-col>
             </el-row>
+
+            <el-dialog
+                :visible.sync="visible"
+                width="30%"
+                @close="close('form')"
+            >
+                <span slot="title">{{title}}</span>
+                <div class="dialog-body">
+                    <p>为了保障您的账号安全，变更信息前需验证身份</p>
+                    <el-form ref="form" :model="form" label-position="top" :rules="rules">
+                        <el-form-item label="验证方式">
+                            <el-select
+                                v-model="form.region"
+                                placeholder="请选择验证方式"
+                                size="medium"
+                            >
+                                <el-option :label="telephone" value="shanghai"></el-option>
+                                <el-option :label="email" value="beijing"></el-option>
+                            </el-select>
+                        </el-form-item>
+
+                        <el-form-item prop="verify">
+                            <el-row type="flex" justify="space-between">
+                                <el-col :span="13">
+                                    <el-input v-model="form.verify" autocomplete="off"></el-input>
+                                </el-col>
+                                <el-col :span="10">
+                                    <el-button plain style="width: 100%">发送验证码</el-button>
+                                </el-col>
+                            </el-row>
+                        </el-form-item>
+                    </el-form>
+                </div>
+
+                <span slot="footer" class="dialog-footer">
+                    <el-button type="primary" @click="submit('form')" style="width: 100%">确 定</el-button>
+                </span>
+            </el-dialog>
         </div>
     </el-main>
 </template>
@@ -95,6 +137,13 @@
     export default {
         name: "AccountSetting",
         data(){
+            let validateVerify = function(rule, value, callback){
+                if(value === ''){
+                    callback(new Error('请填写验证码'));
+                } else {
+                    callback();
+                }
+            }
             return {
                 sex:true,
                 settings:{
@@ -106,7 +155,18 @@
                 user: {
                     nickname: this.$store.state.user.nickname,
                     address: this.$store.state.user.address,
-                }
+                },
+                visible:false,
+                form:{
+                    region:'',
+                    verify:''
+                },
+                rules:{
+                    verify:[
+                        {validator: validateVerify, trigger: 'blur'}
+                    ]
+                },
+                title:''
             }
         },
 
@@ -116,11 +176,68 @@
                     this.$store.commit('changeInformation',{key: name, value: this.$refs[name].value});
                 }
                 this.settings[name] = !this.settings[name];
+            },
+
+            submit(name){
+                this.$refs[name].validate(valid => {
+                   if(valid){
+                       console.log('submit');
+                       this.visible = false;
+                   }
+                });
+            },
+
+            close(name){
+                this.$refs[name].resetFields();
+            },
+
+            openDialog(title){
+                this.title = title;
+                this.visible = true;
+            }
+        },
+
+        computed: {
+            telephone() {
+                return this.$store.state.user.telephone.replace(/(\d{3})\d{6}(\d{2})/, '$1****$2')+' 手机';
+            },
+
+            email() {
+                return this.$store.state.user.email.replace(/(\w{3})(\w+)(\w{2})(@\w+\.[a-z]+(.[a-z]+)?)/, '$1****$3$4')+' 邮箱';
             }
         }
     }
 </script>
 
 <style scoped>
+    .el-select{
+        width: 100%;
+    }
 
+    /deep/ .el-dialog__body{
+        padding-top: 10px;
+    }
+
+    /deep/ .el-dialog__footer{
+        text-align: center;
+    }
+
+    /deep/ .el-form-item__label{
+        font-weight: 600;
+    }
+
+    .el-form{
+        padding-top: 10px;
+    }
+
+    .dialog-body{
+        padding-left: 10px;
+        padding-right: 10px
+    }
+
+    .dialog-footer{
+        display: block;
+        margin-left: 10px;
+        margin-right: 10px
+    }
 </style>
